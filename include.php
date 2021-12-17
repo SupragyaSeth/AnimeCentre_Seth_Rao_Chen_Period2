d66d34ca38b2a2a821577e8ac66e6213a0dd9a9a
Aditya Rao <94478537+Fireadi107@users.noreply.github.com>
2021-12-17 08:53:07 -0800

    Delete include.php

--- include.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once 'https://github.com/SupragyaSeth/AnimeCentre_Seth_Rao_Chen_Period2/blob/main/Homepage.html'; //add template
-require_once 'login.php';  //add login credentials
-$conn = mysqli_connect($host, $user, $pass, $db, $port); //create connection to db
-if($conn->connect_error) die($conn->connect_error); // quit if it fails ?php>
-?>
